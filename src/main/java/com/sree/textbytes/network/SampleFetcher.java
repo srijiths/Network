b3e7095f7a1dea5bbce279469580634570430c2e
Sreejith.S <sreejiths@algotree.com>
2012-10-12 11:07:21 +0530

    Sample Fetcher class added

--- src/main/java/com/sree/textbytes/network/SampleFetcher.java
+++ src/main/java/com/sree/textbytes/network/SampleFetcher.java
@@ -1,17 +1,10 @@
 package com.sree.textbytes.network;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args ) throws Exception
-    {
-        System.out.println( "Hello World!" );
-        
-        HtmlFetcher htmlFetcher = new HtmlFetcher();
+public class SampleFetcher {
+	public static void main(String[] args) throws Exception {
+		HtmlFetcher htmlFetcher = new HtmlFetcher();
         String html = htmlFetcher.getHtml("http://www.washingtontimes.com/news/2012/aug/29/american-scene-cdc-says-west-nile-cases-rise-40-in/?page=1&utm_medium=RSS&utm_source=RSS_Feed", 0);
         System.out.println("Html : "+html);
-    }
+	}
+
 }
